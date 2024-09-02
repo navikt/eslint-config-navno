30bbc77a198b52eea127d33b219290dcfa5676ff
Terje Karlsen <terjeofnorway@gmail.com>
2024-09-02 17:17:02 +0200

    Adjust prettier config with semi

--- prettier/index.js
+++ prettier/index.js
@@ -1,16 +1,17 @@
 module.exports = {
-  semi: false,
-  trailingComma: "all",
-  singleQuote: true,
-  printWidth: 120,
-  tabWidth: 4,
-  overrides: [
-    {
-      files: ["*.yml", "*.yaml"],
-      options: {
-        bracketSpacing: false,
-        tabWidth: 2,
-      },
-    },
-  ],
-};
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 150,
+    tabWidth: 4,
+
+    overrides: [
+        {
+            files: ['*.yml', '*.yaml'],
+            options: {
+                bracketSpacing: false,
+                tabWidth: 2,
+            },
+        },
+    ],
+}
